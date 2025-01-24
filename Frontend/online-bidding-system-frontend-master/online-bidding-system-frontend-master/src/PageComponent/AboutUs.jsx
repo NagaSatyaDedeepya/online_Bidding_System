7d60ceb00d8ecb8d59daa11188b94563fceef693
DedeepyaGelli <121683311+DedeepyaGelli@users.noreply.github.com>
2025-01-24 12:05:15 +0530

    Initial commit

---
+++ Frontend/online-bidding-system-frontend-master/online-bidding-system-frontend-master/src/PageComponent/AboutUs.jsx
@@ -0,0 +1,41 @@
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <h4 className="mb-3">About Us</h4>
+      <b>
+        Online Bidding is a process whereby consumers directly buy goods,
+        services etc. from a seller without an intermediary service over the
+        Internet. Customers can visit web stores from the comfort of their house
+        and shop as by sitting in front of the computer.Ecommerce, also known as
+        electronic commerce or internet commerce, refers to the buying and
+        selling of goods or services using the internet, and the transfer of
+        money and data to execute these transactions.
+        <br />
+        <br />
+        In existing system Bidding can done in a manual way, the customer has
+        to go for shopping, and then he is having the possibility to choose the
+        products what ever he wants. Selling online also has its advantages when
+        it comes to convincing customers you're the best in the industry. Your
+        website can inform customers about your sales, the quality of your
+        products, and why they should buy from you. You can also show customer
+        reviews, so people know they're buying from a reputable brand. 
+        <br />
+        <br />
+        The Bidding system is fast gaining media for to sale or purchase
+        items from anywhere and anytime. It is basically based on Internet, It
+        is related with B2C (Business to Customer) model and status of the
+        design and development of Bidding platform.E-bidding or Online
+        bidding means business transactions that take place online with the
+        help of the internet. The term e-business came into existence in the
+        year 1996. E-bidding is an abbreviation for electronic bidding.
+        Therefore, the buyer and the seller do not meet personally. E-bidding
+        is directly link to your business promotions, as it is the age of
+        digital media. Making your bidding available online is crucial to your
+        business development such as, highly convenience, wide exposure, global
+        customer, easy to run, etc.
+      </b>
+    </div>
+  );
+};
+
+export default AboutUs;
